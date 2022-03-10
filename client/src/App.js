997db223e58f360a43e8bcfdc09dce3eb609ad00
Hari Hara Kumar <hariharakumar7@gmail.com>
2022-03-09 19:25:37 -0600

    Fixed lint issues

--- client/src/App.js
+++ client/src/App.js
@@ -81,7 +81,7 @@ function App() {
         <Route
           path="/login"
           element={
-            <Form
+            <Login
               title="Login"
               setEmail={setEmail}
               setPassword={setPassword}
